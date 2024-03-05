ece50329aad3809e9a7e61658593afe32b3dfad0
Isaac <ioume11@gmail.com>
2024-03-05 07:53:42 -0700

    test

--- studentHelpButton/src/studentHelpButton.cpp
+++ studentHelpButton/src/studentHelpButton.cpp
@@ -10,12 +10,15 @@
 SYSTEM_MODE (SEMI_AUTOMATIC);
 
 //Variables Declared
-int i ;
+int i;
+int buttonStatus1 = 0; 
+int buttonStatus2 = 0;
+int buttonStatus3 = 0;
 
 //Constants Declared/Defined 
-const int HELPBUTTON = D8;
-const int GETBACKTOWORK = D9;
-const int VACATION = D16;
+const int HELPBUTTONPIN = D8;
+const int GETBACKTOWORKPIN = D9;
+const int VACATIONPIN = D16;
 const int LEDPIN = SCL;
 const int SERVOPIN = D15;
 const int LAVAPIN = D17;
@@ -25,6 +28,7 @@ const int DINGPIN = D14;
 
 
 //Declare Objects
+Servo myServo; // Declares the servo object globally
 
 button_click HELPBUTTON (D8);
 button_click GETBACKTOWORK ( D9);
@@ -32,11 +36,54 @@ button_click VACATION (D16);
 
 // setup() runs once, when the device is first turned on
 void setup() {
+  Serial.begin(9600);
+  pinMode (HELPBUTTONPIN, INPUT_PULLUP);
+  pinMode (GETBACKTOWORKPIN, INPUT_PULLUP);
+  pinMode (VACATIONPIN, INPUT_PULLUP);
+
+  pinMode (LEDPIN, OUTPUT);
+  pinMode (LAVAPIN, OUTPUT);
+  pinMode (FANPIN, OUTPUT);
+  pinMode (BUZZERPIN, OUTPUT);
+  pinMode (DINGPIN, OUTPUT);
+  pinMode (SERVOPIN, OUTPUT);
+  myServo.attach(SERVOPIN);
  //  Put initialization like pinMode and begin functions here
 }
 
-// loop() runs over and over again, as quickly as it can execute.
 void loop() {
+  buttonStatus1 = digitalRead(HELPBUTTONPIN);
+  buttonStatus2 = digitalRead(GETBACKTOWORKPIN);
+  buttonStatus3 = digitalRead(VACATIONPIN);
+
+  if (buttonStatus1 == LOW){
+    digitalWrite(LEDPIN, HIGH);
+    Serial.printf("s = %s\n","Help button pressed, Instructor notiifed.");
+  }
+  if (buttonStatus2 == LOW) {
+    digitalWrite (LEDPIN, LOW);
+    Serial.printf("s =%s\n", "Back To Work");
+  }
+  if (button_status3 == LOW) {
+    openCookieJar ();
+    digitalWrite (FANPIN, HIGH);
+    digitalWrite (LAVAPIN, LOW);
+    Serial.printf("s = %s\n", "Don't Worry Be Happy!");
+  } else {
+    closeCookieJar();
+    digitalWrite(FANPIN, LOW);
+    digitalWrite(LAVAPIN, LOW);
+  }
+}
+void openCookieJar(){
+  myServo.write(0);
+  delay(1000);
+}
+void closeCookieJar(){
+  myServo.write(0);
+  delay(1000);
+  
+}
   // The core of your code will likely live here.
 
   // Example: Publish event to cloud every 10 seconds. Uncomment the next 3 lines to try it!
