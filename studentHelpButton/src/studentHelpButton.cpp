d707bdd9e960b63db20ed5f4ab52d684badeb326
Isaac <ioume11@gmail.com>
2024-03-07 16:28:05 -0700

    No Errors missing auto buzz march 7, 2024 1628hours

--- studentHelpButton/src/studentHelpButton.cpp
+++ studentHelpButton/src/studentHelpButton.cpp
@@ -1,7 +1,9 @@
 /*
- * Project: studentHelpButton
+ * Project: Student Help Button
  * Author: Isaac Martinez Sr.
  * Date: 3-4-24
+ * Description: This project is designed to help students notify instructors when they need assistance, 
+ * and also includes features like vacation mode for automatic handling of certain tasks.
  */
 
 #include "Particle.h"
@@ -12,171 +14,131 @@
 #include "wemo.h"
 #include "hue.h"
 
-SYSTEM_MODE(MANUAL); //control logging into classroom router
+SYSTEM_MODE(MANUAL); // Controls logging into classroom router without using automatic connection
 
-//Variables Declared
-int i;
+// Variable Declarations
+char studentName[] = "Isaac";
 bool buttonOnOff = false;
 bool vacationMode = false;
-int color;
 
-char studentName [] ={"Isaac"};
 
-//Constants Declared/Defined
-//const int HELPBUTTONPIN = D8; 
-//const int GETBACKTOWORKPIN = D9;
-//const int VACATIONPIN = D16;
+
+// Pin Constants
 const int LEDPIN = SCL;
 const int SERVOPIN = D15;
-const int LAVAPIN = D17;
-const int FANPIN = D18;
 const int BUZZERPIN = D19;
 const int DINGPIN = D14;
 const int MYWEMO = 1;
 const int BULB = 6;
 const int BRIGHTNESS = 100;
-const int initDing =3000;
-const int recurDing =300000;
+const int recurDing = 300000;
 
-
-
-//Declare Objects
-Servo myServo; // Declares the servo object globally
+// Object Declarations
+Servo myServo;
 Adafruit_SSD1306 display(-1);
-IoTTimer buzzerTimer;
 IoTTimer startDingTimer;
 Button helpButton(D8);
 Button backToWorkButton(D9);
 Button vacationButton(D16);
-// class, object, (pin/sometime nothing)
+
 // Function prototypes
-void openCookieJar();
 void closeCookieJar();
 //void displaySplashScreen();
 void buzzerCallback();
 void dingCallback();
+void openCookieJar();
 
 void setup() {
   Serial.begin(9600);
+
+  // Initialize Wi-Fi connection
   WiFi.on();
   WiFi.clearCredentials();
   WiFi.setCredentials("IoTNetwork");
   WiFi.connect();
-  while(WiFi.connecting()) {
-    Serial.printf("Lets Go!!!!");
+  while (WiFi.connecting()) {
+    Serial.print("Connecting to WiFi...");
   }
+  Serial.println("Connected!");
 
+  // Pin Modes
   pinMode(LEDPIN, OUTPUT);
-  //pinMode(LAVAPIN, OUTPUT);
-  //pinMode(FANPIN, OUTPUT);
   pinMode(BUZZERPIN, OUTPUT);
   pinMode(DINGPIN, OUTPUT);
   pinMode(SERVOPIN, OUTPUT);
+
+  // Attach servo
   myServo.attach(SERVOPIN);
 
-  // Initialize OLED
+  
+  
+
+
+  // Initialize OLED Display
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   display.setRotation(2);
   display.setTextSize(1);
   display.setTextColor(WHITE);
-  display.setCursor(0, 0);
   display.clearDisplay();
-  display.display();
-  delay(2000);
+  display.setCursor(0, 0);
   display.println("Student Help Button");
   display.println("By Isaac Martinez Sr.");
   display.display();
   delay(2000);
   display.clearDisplay();
-
-  // Initialize Ding
 }
 
 void loop() {
-    
+  // Help Button Pressed
   if (helpButton.isClicked()) {
-    display.setCursor(0, 0);
-    setHue(BULB, true, HueRed, BRIGHTNESS, 255);
-    Serial.printf("Help button pressed, Instructor notified %s is now in the que\n",studentName);
-    display.printf("Help button pressed, Instructor notified %s is now in the que\n",studentName);
-    tone(DINGPIN,4186,2000); 
-    Serial.printf("Buzzer on\n");
-    startDingTimer.startTimer(recurDing);
+    display.printf("Help button pressed, Instructor notified. You are now in the queue.");
     display.display();
+    setHue(BULB, true, HueRed, BRIGHTNESS, 255);
+    tone(DINGPIN, 4186, 2000); // Play a tone for notification
+    startDingTimer.startTimer(recurDing);
     display.clearDisplay();
 
-  } 
-    if(startDingTimer.isTimerReady()){
-      tone(DINGPIN,4186,3000); 
-      startDingTimer.startTimer(recurDing);
-    }
-       if (backToWorkButton.isClicked()) {
-        display.setCursor(0, 0);
-    setHue(BULB, false, HueRed, BRIGHTNESS, 255);
-        display.setCursor(0, 0);
-    display.printf("Get Back To Work!,%s has been saved.\n",studentName);
-
-    }
-   
-  if (vacationButton.isClicked()){
-    buttonOnOff =! buttonOnOff; 
   }
-  if(buttonOnOff){
-    display.setCursor(0, 0);
-    wemoWrite (MYWEMO, HIGH);
-    openCookieJar();
-    display.setCursor(0, 0);
-display.printf("Test Message\n");
+
+  // Repeat Notification
+  if (startDingTimer.isTimerReady()) {
+    tone(DINGPIN, 4186, 3000);
+    startDingTimer.startTimer(recurDing);
+  }
+
+  // Back to Work Button Pressed
+  if (backToWorkButton.isClicked()) {
+    display.printf("Get Back To Work! You have been saved.");
     display.display();
+    setHue(BULB, false, HueRed, BRIGHTNESS, 255);
+  }
 
-
-   // Serial.printf("Don't Worry Be Happy!");
-   // display.printf("Don't Worry Be Happy!");
+  // Vacation Mode Toggle
+  if (vacationButton.isClicked()) {
+    buttonOnOff = !buttonOnOff;
+    if (buttonOnOff) {
+      wemoWrite(MYWEMO, HIGH);
+      openCookieJar();
+      display.printf("Vacation mode ON. Enjoy your break!");
+    } else {
+      wemoWrite(MYWEMO, LOW);
+      closeCookieJar();
+      display.printf("Vacation mode OFF. Welcome back!");
+    }
+  }
 }
-  else{ 
-    display.setCursor(0, 0);
-    wemoWrite(MYWEMO,LOW);
-    closeCookieJar();
-  }
-  }
-  
 
-
-   
-
-
-    
-
-
-//   // Control Hue light based on help button state
-//   if (buttonOnOff) {
-//     setHue(BULB, true, HueRainbow[color % 7], BRIGHTNESS, 255);
-//   } else {
-//     setHue(BULB, false, HueRainbow[color % 7], BRIGHTNESS, 255);
-//   }
-
-// }
-
-void openCookieJar(){
-  myServo.write(360);
-  //delay (1000);
+ void openCookieJar() {
+  myServo.write(360); // Open the jar
 }
 
 void closeCookieJar() {
-  myServo.write(0);
-  //delay(1000);
+  myServo.write(0); // Close the jar
 }
 
-void buzzerCallback() {
-  digitalWrite(BUZZERPIN, HIGH);
-  delay(1000);
-  digitalWrite(BUZZERPIN, LOW);
-}
-
-void dingCallback() {
-  if (buttonOnOff) {
-    digitalWrite(DINGPIN, HIGH);
-    delay(500);
-    digitalWrite(DINGPIN, LOW);
-  }
+void displayMessage(const char* message) {
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.println(message);
+  display.display();
 }
